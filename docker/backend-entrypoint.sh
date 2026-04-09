0d0865d022054cd229613a3916f20853ccda7f1c
ivan lambev <ivan2007ny@gmail.com>
2026-04-09 13:38:33 +0300

    Handle legacy schema when bootstrapping Alembic

--- docker/backend-entrypoint.sh
+++ docker/backend-entrypoint.sh
@@ -2,6 +2,35 @@
 set -eu
 
 if [ "${ADND_RUN_MIGRATIONS_ON_STARTUP:-1}" = "1" ]; then
+  LEGACY_SCHEMA_STATE="$(python - <<'PY'
+import os
+
+import psycopg
+
+conn = psycopg.connect(
+    host=os.environ.get("ADND_POSTGRES_HOST", "postgres"),
+    port=os.environ.get("ADND_POSTGRES_PORT", "5432"),
+    dbname=os.environ.get("ADND_POSTGRES_DB", "ad_neuro_diagnostics"),
+    user=os.environ.get("ADND_POSTGRES_USER", "adnd_app"),
+    password=os.environ["ADND_POSTGRES_PASSWORD"],
+)
+try:
+    with conn.cursor() as cur:
+        cur.execute("SELECT to_regclass('public.analysis_jobs'), to_regclass('public.alembic_version')")
+        analysis_jobs, alembic_version = cur.fetchone()
+        if analysis_jobs and not alembic_version:
+            print("stamp")
+        else:
+            print("upgrade")
+finally:
+    conn.close()
+PY
+)"
+
+  if [ "$LEGACY_SCHEMA_STATE" = "stamp" ]; then
+    alembic stamp head
+  fi
+
   alembic upgrade head
 fi
 
